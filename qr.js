c3b2f016f3957ef1d73891a100a62e767feb3ea1
Thilina30599 <79817669+Thilina30599@users.noreply.github.com>
2021-08-28 18:33:06 +0530

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,44 @@
+/* Copyright (C) 2020 Yusuf Usta.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsena - Yusuf Usta
+*/
+
+const chalk = require('chalk');
+const {WAConnection} = require('@adiwajshing/baileys');
+const {StringSession} = require('./whatsasena/');
+const fs = require('fs');
+
+async function whatsAsena () {
+    const conn = new WAConnection();
+    const Session = new StringSession();  
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 30000;
+    
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+${ chalk . white . italic ( 'AsenaString Code Recipient' ) }
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+    });
+    
+
+    conn.on('open', () => {
+        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
+        console.log(
+            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+        );
+        
+        if (!fs.existsSync('config.env')) {
+            fs.writeFileSync('config.env', `Thilina Session="${st}"`);
+        }
+
+        console.log(
+            chalk . blue . bold ( 'If you are setting up a locale, you can start the bot with node bot.js.' )
+        );
+        process.exit(0);
+    });
+
+    await conn.connect();
+}
+
+whatsAsena ( )
