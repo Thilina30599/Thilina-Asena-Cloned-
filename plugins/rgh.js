8afab1a0e23a02229da40f7ac26dc527c708ab86
Thilina30599 <79817669+Thilina30599@users.noreply.github.com>
2021-08-30 09:43:20 +0530

    Create rgh.js

---
+++ plugins/rgh.js
@@ -0,0 +1,13 @@
+/*Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'emergency', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage(".spam 🤖𝓣𝓱𝓲𝓵𝓲𝓷𝓪 𝓑𝓸𝓽🤖* *ᴀʟɪᴠᴇ ɴᴏᴡ*");
+
+}))
