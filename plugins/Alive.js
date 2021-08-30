e0f1321a9a69e319d38bed92d478c3659527f060
Thilina30599 <79817669+Thilina30599@users.noreply.github.com>
2021-08-30 08:34:35 +0530

    Create Alive.js

---
+++ plugins/Alive.js
@@ -0,0 +1,13 @@
+/*Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'workingbot', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("*Bang!! Bang!! My Bot is 🤖𝓣𝓱𝓲𝓵𝓲𝓷𝓪 𝓑𝓸𝓽🤖*");
+
+}));
